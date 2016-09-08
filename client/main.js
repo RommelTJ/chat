cd9d5196299ef6124148bab5135b71240af4efd4
Rommel Rico <rommeltj@gmail.com>
2016-09-07 19:43:40 -0700

    Renaming app from hello to chat

--- client/main.js
+++ client/main.js
@@ -3,18 +3,18 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.chat.onCreated(function chatOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.chat.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.chat.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
