504ad8328c30d111e2d91b56b02120f4dca62607
Rommel Rico <rommeltj@gmail.com>
2016-09-08 19:26:56 -0700

    Meteor Session & Reactivity

--- client/main.js
+++ client/main.js
@@ -4,13 +4,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.chat.onCreated(function chatOnCreated() {
-  // counter starts at 0
+  // counter starts at 0 (default)
   this.counter = new ReactiveVar(0);
 });
 
 Template.chat.helpers({
   counter() {
-    return Template.instance().counter.get();
+    return Template.instance().counter.get(); //Retrieve session.
   },
   doubleNumber(num) {
     return num*2;
@@ -20,6 +20,6 @@ Template.chat.helpers({
 Template.chat.events({
   'click .clickable'(event, instance) {
     // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+    instance.counter.set(instance.counter.get() + 1); //Set session.
   },
 });
